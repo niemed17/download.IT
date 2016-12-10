dc4775b8cea0c2480d53417990ae20ce8b487988
zyllar68 <zyrilmancion@gmail.com>
2016-12-10 20:48:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+
+	<!DOCTYPE html>
+
+
+
+	<html>
+
+		<head>
+		 	<meta charset = "utf-8">
+
+				<title>Home</title>
+
+				<link rel = "stylesheet" type = "text/css" href="libraries/semantic/semantic.min.css">
+				<link rel="stylesheet" type="text/css" href="Libraries/semantic_ui_css.css">
+				<link rel="stylesheet" type="text/css" href="Libraries/semantic_ui_modal.css">
+				<script src="libraries/semantic/semantic.js"></script>
+				<script src="libraries/semantic/semantic.min.js"></script>
+				<script src="Libraries/jquery_1.11.1.js"></script>
+				<script src="Libraries/semantic_ui_ajax.js"></script>
+				<script src="Libraries/semantic_ui_ajax_modal.js"></script>
+
+				<style>
+
+					*{ font-family: 'Gotham'; }
+
+				</style>
+
+		</head>
+
+		<body>
+
+			<div class="ui inverted menu">
+			  <div class="header item">Brand</div>
+			  <div class="active item">Link</div>
+			  <a class="item">Link</a>
+			  <div class="ui dropdown item" tabindex="0">
+			    Dropdown
+			    <i class="dropdown icon"></i>
+			    <div class="menu transition hidden" tabindex="-1">
+			      <div class="item">Action</div>
+			      <div class="item">Another Action</div>
+			      <div class="item">Something else here</div>
+			      <div class="divider"></div>
+			      <div class="item">Separated Link</div>
+			      <div class="divider"></div>
+			      <div class="item">One more separated link</div>
+			    </div>
+			  </div>
+			  <div class="right menu">
+			    <div class="item">
+			      <div class="ui transparent inverted icon input">
+			        <i class="search icon"></i>
+			        <input type="text" placeholder="Search">
+			      </div>
+			    </div>
+			    <a class="item">Link</a>
+			  </div>
+			</div>
+
+
+		</body>
+
+
+	</html>

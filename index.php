4e4a332354b7454fcae05acf25faf4ff5f5e1a63
zyllar68 <zyrilmancion@gmail.com>
2016-12-11 16:51:53 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -24,20 +24,23 @@
 				<style>
 
 					*{
-		font-family: 'Open Sans', sans-serif;
-	}
-					body {
-					  text-align: center;
+
+						font-family: 'Gotham',;
+
+					}
+					.body {
+
+				 		 text-align: center;
+						 width: 100%;
+						 height: 660px;
+						 background-image: url('Assets/yeah.jpg');
+						 background-size: 100% 100%;
+					     background-repeat: no-repeat;
+					 
 					 }
 
 
-					 #img {
-					width: 100%;
-					height: 500px;
-					background-image: url('Assets/yeah.jpg');
-					background-size: 100% 100%;
-					border: 1px solid red;
-}						
+		
 				</style>
 
 		</head>
@@ -46,51 +49,144 @@
 				
 <body>
   
-		<div class="ui fixed menu" id="page-wrap" style="background-color:rgba(127,64,136,0.8);">
+	<div class="body">
+		
+		<div class="ui container">	
 
-				
-			<div class="ui secondary menu">
+			<div class="ui fixed menu" id="page-wrap" style="background-color:rgba(127,64,136,0.8); position: absolute; padding: 10px;">
 
-				<img src="assets/download.png" style="margin-top:15px; margin-left:200px; height: 30px; width: 30px;">
+				<div class="ui secandary meny">
+						
+					<p style="position: relative; font-size:25px; color: white; margin-top: 1%;">
 
-				<div  style=" margin-top: 15px; margin-left: 10px; color: white;" >
-								<p style="font-size:20px; font-weight: 500;">DOWNLOAD.IT</p>
+						<img src="assets/download.png" style="margin-top:8px; margin-left:200px; height: 25px; width: 30px;">
+
+					<b>DOWNLOAD.IT</b></p>
+										
+				</div>
+
+
+				<div class="right menu">
+					
+					<div class="item">
+					<a href="#" style="color:white; font-weight:500; font-size:15px; margin-right: 50px;">FEATURES</a>
+					</div>
+
+					<div class="item">
+					<a href="#" style="color:white; font-weight:500; font-size:15px; margin-right: 50px;">PLANS</a>
+					</div>
+
+					<div class="item">
+					<a href="#" style="color:white; font-weight:500; font-size:15px; margin-right:200px;">LOGIN</a>
+					</div>
 
 				</div>
 
 			</div>
-
-			<div class="ui right secondary menu" style="text-align: right; margin-top: 19px;">
-				<a href="#" style="color:white; font-weight:500; font-size:15px; margin-right: 50px;">FEATURES</a>
-				<a href="#" style="color:white; font-weight:500; font-size:15px; margin-right: 50px;">PLANS</a>
-				<a href="#" style="color:white; font-weight:500; font-size:15px; margin-right:200px;">LOGIN</a>
-			</div>
 				
-		</div>
+			
+				<div class="ui grid container">
 
+					<div class="sixteen wide column"></div>
+					<div class="sixteen wide column"></div>
 
+					<div class="sixteen wide column">
 
-					<div >
+						<p style="color:white; font-size: 30px; font-weight:900;">TRANSFER YOUR FILES EASILY, FAST AND SECURE</p>
 
-					<div id="img">
-					<p style="font-weight: 700;  font-size: 30px;  color:white; text-align: center; position:relative; top:110px;">TRANSFER YOUR FILES EASILY, FAST AND SECURE</p>
-					<p style="font-weight: 500;  font-size: 20px;  color:white; text-align: center; position:relative; top:90px;">Access your files, anytime, anywhere with our revolutionary files platform</p>
+						<p style="color:white; font-size: 20px; margin-top:-2%;"><i>Access your files, anytime, anywhere with our revolutionary files platform</i></p>
+					</div>
+	
+					
+				</div>
+
+				<div class="ui centered grid">
+
+					<div class="five wide column">	
+
+						<div class="ui segments" style="border: dashed; border-color: white; border-radius: 5px; height: auto; padding: 10px;">
+								
+								<div class="ui segment" style="border-radius: 5px; height: 300px;">
+									
+									<p style="text-align: center; font-weight: 600; font-size: 25px;"">DRAG AND DROP</p>
+
+								</div>
+
+						</div>
 
 					</div>
-					
-							
+
+				</div>
+
+				<div class="ui centered grid">	
+
+					<div class="four wide column">
+
+					<button class="ui inverted button" style="width: 270px; height: 50px; font-weight: 600; border-radius: 9px;">SIGN UP FOR FREE</button>
+
+					</div>
+
+				</div>
+
+		</div>
+
+	</div>
 
 
-							
-					
-		   			</div>
+<div class="ui grid" style="margin: 8% 0% 0% 0%;">
 
-					
+	
+		<div class="one wide column"></div>
+			
+		
+		<div class="fourteen wide column" style="font-size: 20px; margin: 0% 10% 0% 14%;">
+			
+			<img src="Assets/laptop.jpg" style="height: 120%; float: right;">
 
+			<p style="margin-left: 7%;">Easy share, no hassle</p>
+			<p style="margin-left: 7%;">Download.it provides an easy  way to share<br />
+			 files, with a copy-paste link of your file URL,<br />
+			  you can easily share the link to anyone you<br />
+			  want to have access to the files</p>
 
+			  <div class="ui black basic button"  style="width: 20%; margin: 0% 0% 0% 7%;">Start Sharing</div> 
+			 
+		</div>
 
+		<div class="one wide column"></div>
+				
+		
+		
+	
+</div>
 
-  
+	<div class="ui grid" style="margin: 8% 0% 0% 0%;">
+
+		
+			<div class="one wide column"></div>
+				
+			
+			<div class="fourteen wide column" style="font-size: 20px; margin: 0% 10% 0% 16%;">
+				
+				<img src="Assets/iphone.png" style="height: 60%; float: left;">
+
+				<p>Share anywhere on the go</p>
+				<p>Download.it is available in mobile for iOS<br />
+					and Android. Experience convenience and <br />
+					functionality in one package, download our<br />
+					mobile application to experience.</p>
+
+				  <div class="ui black basic button"  style="width: 20%;">Download for Mobile </div> 
+				 
+			</div>
+
+			<div class="one wide column"></div>
+
+	</div>
+
+  	<div class="ui grid">
+  		
+  	</div>
 </body>
 
 						
